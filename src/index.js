7adc65f412eba134321fa69cc6ebc64c3ea70b37
Your Name <you@example.com>
2022-08-11 12:48:54 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix===undefined) {
+    return []
+  }
+
+  else {
+  const sorted =  matrix.reduce((acc, cur, i) => {
+   return acc.concat((!(i % 2) ? cur : cur.reverse()));
+  }, []);;
+  return sorted 
+};};
+
